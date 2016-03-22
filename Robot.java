d00d620e55625294aa71aad4549ce7ebc71e39ea
XpRage <leggiorobbie@gmail.com>
2016-03-22 13:41:17 -0700

    Updated with an encoder
    This allows us to have a smooth autonomous and an easy tele-op. This
    updated version is different on the way the tomohawks react and how the
    robot will benefit us with a encoder on the tomohawk.

--- Robot.java
+++ Robot.java
@@ -13,6 +13,8 @@ import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 import edu.wpi.first.wpilibj.CANTalon.TalonControlMode;
 import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+import edu.wpi.first.wpilibj.Encoder;
 
 
 /**
@@ -24,15 +26,22 @@ import edu.wpi.first.wpilibj.CameraServer;
  */
 public class Robot extends IterativeRobot {
     
+	
 	final String defaultAuto = "Default";
     final String RoughTerrainAuto = "RoughTerrainAuto";
     final String RampartsAuto = "RampartsAuto";
     final String MoatAuto = "MoatAuto";
     final String WallAuto = "WallAuto";
+    final String PortcullisAuto = "PortcullisAuto";
+    final String LowbarAuto = "LowbarAuto";
     
+    //final String resetEncoder = "resetEncoder";
+    //final String liveEncoder = "liveEncoder" ;
     
     String autoSelected;
+    //String autoSelectedButton;
     SendableChooser chooser;
+    SendableChooser button;
     CANTalon _frontLeftMotor = new CANTalon(0); 		/* device IDs here (1 of 2) */
 	CANTalon _rearLeftMotor = new CANTalon(1);
 	CANTalon _frontRightMotor = new CANTalon(2);
@@ -41,6 +50,7 @@ public class Robot extends IterativeRobot {
 	/* extra talons for six motor drives */
 	CANTalon _intake = new CANTalon(15);
 	CANTalon _tomohawk = new CANTalon(14);
+	
 	
 	RobotDrive _drive = new RobotDrive(_frontLeftMotor, _rearLeftMotor, _frontRightMotor, _rearRightMotor);
 	
@@ -66,18 +76,22 @@ public class Robot extends IterativeRobot {
         chooser.addObject("Rough Terrain", RoughTerrainAuto);
         chooser.addObject("Ramparts", RampartsAuto);
         chooser.addObject("Moat", MoatAuto);
-        chooser.addObject("WallAuto", WallAuto);
-        
+        chooser.addObject("RockWall", WallAuto);
+        chooser.addObject("Portcullis", PortcullisAuto);
+        chooser.addObject("LowBar", LowbarAuto);
         
         SmartDashboard.putData("Auto choices", chooser);
         //_leftSlave.changeControlMode(TalonControlMode.Follower);
     	//_rightSlave.changeControlMode(TalonControlMode.Follower);
     	//_leftSlave.set(11); 							/* device IDs here (2 of 2) */
     	//_rightSlave.set(14);
-    
-    		
-    	
-    	
+        
+      /**  button = new SendableChooser();
+        button.addDefault("Default button", defaultAuto);
+    	button.addObject("liveEncoder", liveEncoder);
+    	button.addObject("resetEncoder", resetEncoder);
+    	SmartDashboard.putData("Reset Encoder ", button);
+    	*/
     	server.startAutomaticCapture();
     	/* the Talons on the left-side of my robot needs to drive reverse(red) to move robot forward.
     	 * Since _leftSlave just follows frontLeftMotor, no need to invert it anywhere. */
@@ -100,6 +114,7 @@ public class Robot extends IterativeRobot {
     	autoState = 0;
     	
     	autoSelected = (String) chooser.getSelected();
+    	
 //		autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
 		System.out.println("Auto selected: " + autoSelected);
     }
@@ -110,14 +125,14 @@ public class Robot extends IterativeRobot {
     	{
     	case 0:
     		// Rough Terrain code 
-    		
+    		_tomohawk.setEncPosition(1345);
     		_rearRightMotor.set(-.52);;
     		_rearLeftMotor.set(-.50);;
     		_frontRightMotor.set(-.50);;
     		_frontLeftMotor.set(-.50);;
     		
     		// got to state 2 when counter = 100
-    		if (autoCounter >= 103.5)
+    		if (autoCounter >= 109.87)
     		{
     			
     			autoState = 1;
@@ -153,13 +168,68 @@ public class Robot extends IterativeRobot {
     	
     }
 
+    public void Run_Lowbar_auto()
+    {
+    	switch(autoState)
+    	{
+    	case 0:
+    		// Rough Terrain code 
+    		_tomohawk.setEncPosition(1345);
+    		_rearRightMotor.set(-.52);;
+    		_rearLeftMotor.set(-.50);;
+    		_frontRightMotor.set(-.50);;
+    		_frontLeftMotor.set(-.50);;
+    		_tomohawk.set(-.40);
+    		
+    		// got to state 2 when counter = 100
+    		if (_tomohawk.getEncPosition() <= 730)
+    		{
+    	      _tomohawk.set(0);
+    		}
+    		if (autoCounter >= 109.87)
+    		{
+    			
+    			autoState = 1;
+    			autoCounter = 0;
+    			
+    		}
+    		break;
+    		
+    	case 1:
+    		_rearRightMotor.set(.30);;  // breaking for after rough terrain
+    		_rearLeftMotor.set(.30);;
+    		_frontRightMotor.set(.30);;
+    		_frontLeftMotor.set(.30);;
+    		if(autoCounter >= 25)
+    		{
+    			autoState = 2;
+    		    autoCounter = 0;
+    		}
+    			
+    		   
+    		break;
+    	case 2:
+    		_rearRightMotor.set(0);;
+    		_rearLeftMotor.set(0);;
+    		_frontRightMotor.set(0);;
+    		_frontLeftMotor.set(0);;
+    		break;
+    		
+    	}
+    		
+    
+    
+    	
+    }
+
+    
     public void Run_Ramparts_Auto()
     {
     	switch(autoState)
     	{
     	case 0:
     		// drive toward ramparts
-    		
+    		_tomohawk.setEncPosition(1345);
     		_rearRightMotor.set(-.72);;
     		_rearLeftMotor.set(-.70);;
     		_frontRightMotor.set(-.70);;
@@ -203,14 +273,14 @@ public class Robot extends IterativeRobot {
     	{
     	case 0:
     		// Rough Terrain code 
-    		
-    		_rearRightMotor.set(-.62);;
-    		_rearLeftMotor.set(-.60);;
-    		_frontRightMotor.set(-.60);;
-    		_frontLeftMotor.set(-.60);;
+    		_tomohawk.setEncPosition(1345);
+    		_rearRightMotor.set(-.72);;
+    		_rearLeftMotor.set(-.70);;
+    		_frontRightMotor.set(-.70);;
+    		_frontLeftMotor.set(-.70);;
     		
     		// got to state 2 when counter = 100
-    		if (autoCounter >= 112.5)
+    		if (autoCounter >= 98.7)
     		{
     			
     			autoState = 1;
@@ -220,11 +290,11 @@ public class Robot extends IterativeRobot {
     		break;
     		
     	case 1:
-    		_rearRightMotor.set(.30);;  // breaking for after rough terrain
-    		_rearLeftMotor.set(.30);;
-    		_frontRightMotor.set(.30);;
-    		_frontLeftMotor.set(.30);;
-    		if(autoCounter >= 25)
+    		_rearRightMotor.set(.40);;  // breaking for after rough terrain
+    		_rearLeftMotor.set(.40);;
+    		_frontRightMotor.set(.40);;
+    		_frontLeftMotor.set(.40);;
+    		if(autoCounter >= 30)
     		{
     			autoState = 2;
     		    autoCounter = 0;
@@ -250,14 +320,14 @@ public class Robot extends IterativeRobot {
     	{
     	case 0:
     		// Rough Terrain code 
-    		
-    		_rearRightMotor.set(-.62);;
-    		_rearLeftMotor.set(-.60);;
-    		_frontRightMotor.set(-.60);;
-    		_frontLeftMotor.set(-.60);;
+    		_tomohawk.setEncPosition(1345);
+    		_rearRightMotor.set(-.72);;
+    		_rearLeftMotor.set(-.70);;
+    		_frontRightMotor.set(-.70);;
+    		_frontLeftMotor.set(-.70);;
     		
     		// got to state 2 when counter = 100
-    		if (autoCounter >= 110.5)
+    		if (autoCounter >= 125.5)
     		{
     			
     			autoState = 1;
@@ -291,6 +361,73 @@ public class Robot extends IterativeRobot {
     
    }
     
+    public void Run_Portcullis_Auto()
+    {
+    	switch(autoState)
+    	{
+    	case 0:
+    		// Rough Terrain code 
+    		_tomohawk.setEncPosition(1345);
+    		_rearRightMotor.set(-.52);;
+    		_rearLeftMotor.set(-.50);;
+    		_frontRightMotor.set(-.50);;
+    		_frontLeftMotor.set(-.50);;
+    		
+    		// got to state 2 when counter = 100
+    		if (autoCounter >= 55.9)
+    		{
+    			
+    			autoState = 1;
+    			autoCounter = 0;
+    			
+    		}
+    		break;
+    		
+    	case 1:
+    		_rearRightMotor.set(-.30);;  // breaking for after rough terrain
+    		_rearLeftMotor.set(-.30);;
+    		_frontRightMotor.set(-.30);;
+    		_frontLeftMotor.set(-.30);;
+    		_tomohawk.set(.55);
+    		if(_tomohawk.getEncPosition() >= 500)
+    		{
+    		 _tomohawk.set(0);	
+    		}
+    		
+    		if(autoCounter >= 55)
+    		{
+    			autoState = 2;
+    		    autoCounter = 0;
+    		}
+    			
+    		   
+    		break;
+    	case 2:
+    		_rearRightMotor.set(-.42);;
+    		_rearLeftMotor.set(-.40);;
+    		_frontRightMotor.set(-.40);;
+    		_frontLeftMotor.set(-.40);;
+    		if(autoCounter >= 55)
+    		{
+    			autoState = 3;
+    			autoCounter = 0;
+    		}
+    		break;
+    	
+    	case 3:
+    		_rearRightMotor.set(0);
+    		_rearLeftMotor.set(0);
+    		_frontRightMotor.set(0);
+    		_frontLeftMotor.set(0);
+    		break;
+    	}
+    		
+    
+    
+    	
+    }
+ 
+    
         	
 
     
@@ -300,6 +437,7 @@ public class Robot extends IterativeRobot {
     public void autonomousPeriodic() {
     	
     	autoCounter++;
+    	SmartDashboard.putNumber("tomohawkCount", _tomohawk.getEncPosition());
        	switch(autoSelected) {
        	case RoughTerrainAuto:
     		Run_Rough_Terrain_Auto();
@@ -316,6 +454,14 @@ public class Robot extends IterativeRobot {
      	case WallAuto:
      		Run_Wall_Auto();
      		break;
+     	
+     	case PortcullisAuto:
+     		Run_Portcullis_Auto();
+     		break;
+     		
+     	case LowbarAuto:
+     		Run_Lowbar_auto();
+     		break;
        	}
     
     }
@@ -327,36 +473,64 @@ public class Robot extends IterativeRobot {
     	double left = _joy.getRawAxis(1); // logitech gampad left X, positive is forward
     	double right = _joy2.getRawAxis(1); //logitech gampad right X, positive means turn right
     	_drive.tankDrive(left, -right);//(forward, turn);
+    	//autoSelectedButton = (String) button.getSmartDashboardType();
+    	SmartDashboard.putNumber("tomohawkCount", _tomohawk.getEncPosition());
+        
     	if(_xBoxController.getRawAxis(2) == 1)
     	{
     		//intake
-    		_intake.set(1.0);
+    		_intake.set(-1.0);
+    		
     	}
     	else if(_xBoxController.getRawAxis(3) == 1)
     	{
     		//outake
-          _intake.set(-1.0);
+          _intake.set(1.0);
+          
     	}
     	else
     	{
     	 //turn off	
     		_intake.set(0);
     	}
-    	if(_xBoxController.getRawButton(5)){
-    		//forward _tomohawk
-    		_tomohawk.set(0.5);
+    	
+    	/**
+    	 * This Method is used to control 
+    	 * Mid-Evils Tomohawks
+    	 */
+    	if(_xBoxController.getRawButton(5) && _tomohawk.getEncPosition() <= 750){
+    		//upward _tomohawk
+    		_tomohawk.set(0.55);
+    		
     	}
-    	else if(_xBoxController.getRawButton(6))
+    	else if(_xBoxController.getRawButton(6) && _tomohawk.getEncPosition() >= 700)
     	{
-    		//reverse _tomohawk
-    		_tomohawk.set(-0.35);
+    		//downward _tomohawk
+    		_tomohawk.set(-0.40);
     	}
     	else
     	{
     		//turn off
     	  _tomohawk.set(0);	
     	}
+    	
+    	/**
+    	 * this will be used to adjust encoder position at
+    	 * any moment to fix the tomohawk at its highest position or
+    	 * at its lowest position
+    	 */
+    	if(_xBoxController.getRawButton(7))
+    	{
+    	   _tomohawk.setEncPosition(-18);
     	}
+    	else if(_xBoxController.getRawButton(8))
+    	{
+    	   _tomohawk.setEncPosition(1345);	
+    	}
+    	
+    }
+    	
+    	
     	//LiveWindow.run();
     
 
